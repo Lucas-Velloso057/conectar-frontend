12b974a79c4a5561aa2fd75e7349056c96a3cae4
Lucas Velloso <lucas-velloso@hotmail.com>
2025-08-12 17:38:13 -0300

    Correção da geração da build

--- src/views/private/userManager/UserManagerPage.tsx
+++ src/views/private/userManager/UserManagerPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from 'src/contexts/AuthContext';
 import { mockUsers } from 'src/data/mockUsers';
